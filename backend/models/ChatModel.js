79be3c73cbb75e5e85af02ff99b01ad1622780e1
manjuhiremath <manjuhiremath1352@gmail.com>
2024-12-26 15:52:44 +0530

    Create ChatModel.js

---
+++ backend/models/ChatModel.js
@@ -0,0 +1,24 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+
+
+export const Chat = sequelize.define(
+    "chats",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      message: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        // unique: true,
+      },
+
+    }
+  );
+  
+  
+  sequelize.sync();
